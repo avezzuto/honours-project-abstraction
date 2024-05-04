7565dd4fb088905ed93eca443e5a28fa2f73e2b3
Andrea Vezzuto <a.vezzuto@student.tudelft.nl>
2024-05-04 10:25:53 +0200

    Added iris model for reference

---
+++ Outside-the-Box-master/models/iris.py
@@ -0,0 +1,19 @@
+from tensorflow.keras.layers import Convolution2D
+import tensorflow.keras as kr
+from tensorflow.keras import Input
+
+
+def iris(classes, input_shape, weights=None):
+    model = kr.models.Sequential()
+    model.add(Input(shape=(4,)))
+    # Add an initial layer with 4 input nodes, and a hidden layer with 16 nodes.
+    model.add(kr.layers.Dense(16))
+    # Apply the sigmoid activation function to that layer.
+    model.add(kr.layers.Activation("sigmoid"))
+    # Add another layer, connected to the layer with 16 nodes, containing three output nodes.
+    model.add(kr.layers.Dense(3))
+    # Use the softmax activation function there.
+    model.add(kr.layers.Activation("softmax"))
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+    return model
